94ea193637983c12d625c6fd7eedba0a146161bf
Douglas José Barboza <douglasbarbozajm@gmail.com>
2023-05-27 16:12:06 -0300

    Chart logic

--- src/components/charts/Chart.jsx
+++ src/components/charts/Chart.jsx
@@ -12,14 +12,32 @@ ChartJS.register(
   
 const Chart = (props) => {
     let arrayYears = []
-    let yearsCurrent = 0
+    let yearCurrent = 0
+    let objValues = {
+        "Global":{},
+        "Repasse":{},
+        "Contrapartida":{},
+        "Rendimentos":{}
+    }
 
     for(let i = 0;  i < props.data.length; i++){
-        yearsCurrent = props.data[i].ano
-        if(!arrayYears.includes(yearsCurrent))
-            arrayYears.push(yearsCurrent)
+        yearCurrent = props.data[i].ano
+        if(!arrayYears.includes(yearCurrent)){
+            objValues.Global[yearCurrent] = 0
+            objValues.Repasse[yearCurrent] = 0
+            objValues.Contrapartida[yearCurrent] = 0
+            objValues.Rendimentos[yearCurrent] = 0
+
+            arrayYears.push(yearCurrent)
+        }
+        objValues.Global[yearCurrent] += parseInt(props.data[i].valor_global.replace("R$", "").replace(",", ".").replace(".", ""))
+        objValues.Repasse[yearCurrent] += parseInt(props.data[i].valor_repasse.replace("R$", "").replace(",", ".").replace(".", ""))
+        objValues.Contrapartida[yearCurrent] += parseInt(props.data[i].valor_contrapartida.replace("R$", "").replace(",", ".").replace(".", ""))
+        objValues.Rendimentos[yearCurrent] += parseInt(props.data[i].valor_redimentos.replace("R$", "").replace(",", ".").replace(".", ""))
     }
     arrayYears.sort((a,b) => a - b)
+
+    console.log(objValues)
     const labels = arrayYears;
 
     const options = {
@@ -36,25 +54,25 @@ const Chart = (props) => {
         datasets: [
             {
                 label: "Global",
-                data: [8,6,7,5,8],
+                data: Object.entries(objValues.Global).map(([key, value]) => value),
                 borderColor: "rgb(0, 100, 132)",
                 backgroundColor: "rgba(0, 100, 132, 0.5)",
             },
             {
                 label: "Repasses",
-                data: [5,5,7,8,9,5],
+                data: Object.entries(objValues.Repasse).map(([key, value]) => value),
                 borderColor: "rgb(255, 100, 0)",
                 backgroundColor: "rgba(255, 100, 0, 0.5)",
             },
             {
                 label: "Contrapartida",
-                data: [5,5,7,8,9,5],
+                data: Object.entries(objValues.Contrapartida).map(([key, value]) => value),
                 borderColor: "rgb(75, 0, 0)",
                 backgroundColor: "rgba(75, 0, 0, 0.5)",
             },
             {
                 label: "Rendimentos",
-                data: [5,5,7,8,9,5],
+                data: Object.entries(objValues.Rendimentos).map(([key, value]) => value),
                 borderColor: "rgb(0, 110, 25)",
                 backgroundColor: "rgba(0, 110, 25, 0.5)",
             },
